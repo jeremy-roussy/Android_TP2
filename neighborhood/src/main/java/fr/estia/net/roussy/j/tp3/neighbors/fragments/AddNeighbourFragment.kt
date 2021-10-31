c4501ff3523bd3bb73176963c874dd3ef9036cb1
j.roussy <j.roussy@net.estia.fr>
2021-10-31 16:55:34 +0100

    First TP3 commit

--- neighborhood/src/main/java/fr/estia/net/roussy/j/tp3/neighbors/fragments/AddNeighbourFragment.kt
+++ neighborhood/src/main/java/fr/estia/net/roussy/j/tp3/neighbors/fragments/AddNeighbourFragment.kt
@@ -52,15 +52,12 @@ class AddNeighbourFragment : Fragment(), TextWatcher {
     }
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun afterTextChanged(s: Editable?) {
-        TODO("Not yet implemented")
     }
 
     fun isEmailValid(email: CharSequence?): Boolean {
